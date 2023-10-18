e6cfe1d83d843fb8a3e34fea401788368ed0c0fc
OscarAlfaro73 <oscar.alfaro@eurocontrol.int>
2023-10-18 13:36:03 +0200

    changed data source file

--- data_source.R
+++ data_source.R
@@ -1,5 +1,5 @@
 # data file location
 
 data_folder <- 'G:/HQ/dgof-pru/Project/ACE/Data for ACE landing page and digital reports/'
-data_file <- 'hlsr2021_data.xlsx'
-year_report <- 2021
+data_file <- 'hlsr2022_data.xlsx'
+year_report <- 2022
