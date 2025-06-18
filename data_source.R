b8b0ec754ee21db62f92168be148deeb4edc8eb7
GOENS Quinten <quinten.goens@eurocontrol.int>
2025-06-18 15:20:46 +0200

    Updated header

--- data_source.R
+++ data_source.R
@@ -1,5 +1,5 @@
 # data file location
 
-data_folder <- 'G:/HQ/dgof-pru/Project/ACE/Data for ACE landing page and digital reports/'
+data_folder <- '//sky.corp.eurocontrol.int/DFSRoot/Groups/HQ/dgof-pru/Project/ACE/Data for ACE landing page and digital reports/'
 data_file <- 'hlsr2023_data.xlsx'
 year_report <- 2023
